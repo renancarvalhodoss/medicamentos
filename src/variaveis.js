82126b7c61dd639766bd89266b92d3176e4b6d61
renan carvalho <renancarvalhos32@gmail.com>
2021-12-20 01:30:18 -0300

    change variables

--- src/variaveis.js
+++ src/variaveis.js
@@ -1,4 +1,7 @@
-export const URL = "http://localhost:8000/"
+// export const URL = "http://localhost:8000/"
+// export const THIS_URL = "http://localhost:3000/"
+
+export const URL = "https://back-end-medicamentos.herokuapp.com/"
 export const THIS_URL = "http://localhost:3000/"
 
 
